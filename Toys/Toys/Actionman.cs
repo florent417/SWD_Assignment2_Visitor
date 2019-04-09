e5398ba2d1682bdc2f8cf27b5cfe4edc2e71dbaa
florent417 <flole@live.dk>
2019-04-09 11:14:13 +0200

    Started toys

---
+++ Toys/Toys/Actionman.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toys
+{
+    class Actionman : IElement
+    {
+        public string Theme { get; set; }
+        public double Price { get; set; }
+
+        public Actionman()
+        {
+            Theme = "No clothes";
+        }
+
+        public void PrintTheme()
+        {
+            Console.WriteLine("The current theme is: " + Theme);
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+
+        }
+    }
+}
---
+++ Toys/Toys/IElement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toys
+{
+    interface IElement
+    {
+        void Accept(IVisitor visitor);
+    }
+}
